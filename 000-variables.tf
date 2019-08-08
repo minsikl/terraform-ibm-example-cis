843999547e95dae8a323b9feb91234458b0b0b14
Minsik Lee <minsikl@gmail.com>
2019-08-08 13:06:09 -0400

    Create 000-variables.tf

---
+++ 000-variables.tf
@@ -0,0 +1,49 @@
+variable cis_name {
+  description = "Internet Services name"
+  default     = ""
+}
+
+variable cis_plan {
+  description = "Internet Services plan"
+  default     = "free"
+}
+
+variable cis_location {
+  description = "Internet Services location"
+  default     = "global"
+}
+
+variable cis_domain {
+  description = "Internet Services domain. The domain should be a valid domain. Ex) example.com"
+  default     = ""
+}
+
+variable cis_origin1_name {
+  description = "Internet Services origin name for origin1"
+  default     = ""
+}
+
+variable cis_origin1_address {
+  description = "Internet Services origin address for origin1 Ex) 127.0.0.1"
+  default     = ""
+}
+
+variable cis_origin2_name {
+  description = "Internet Services origin name for origin2"
+  default     = ""
+}
+
+variable cis_origin2_address {
+  description = "Internet Services origin address for origin2 Ex) 127.0.0.1"
+  default     = ""
+}
+
+variable cis_origin3_name {
+  description = "Internet Services origin name for origin3"
+  default     = ""
+}
+
+variable cis_origin3_address {
+  description = "Internet Services origin address for origin3 Ex) 127.0.0.1"
+  default     = ""
+}
